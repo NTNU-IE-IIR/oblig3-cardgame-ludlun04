6d1c86e1df88a7669c3c52db004db8c868886f06
ludlun04 <61392837+ludlun04@users.noreply.github.com>
2024-02-26 16:13:01 +0100

    added deal cards to deckofcards

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
@@ -1,4 +1,6 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Scanner;
 public class DeckOfCards {
   private PlayingCard[] cards;
@@ -26,6 +28,18 @@ public class DeckOfCards {
       char suit = this.suits[suitIndex];
       cards[i] = new PlayingCard(suit, value);
     }
+  }
+
+  public Collection<PlayingCard> dealHand(int amountOfCards) {
+    if (amountOfCards < 1 || amountOfCards > this.count) {
+      throw new IllegalArgumentException("Must be in range 1-" + this.count);
+    }
+    ArrayList<PlayingCard> cardsToBeDealt= new ArrayList<>();
+    for (int i = 0; i < amountOfCards; i++) {
+      int randIndex = (int) (Math.random() * (this.count));
+      cardsToBeDealt.add(this.cards[randIndex]);
+    }
+    return cardsToBeDealt;
   }
 
 }
--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/Main.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/Main.java
@@ -1,10 +1,15 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 
+import java.util.Collection;
 import javafx.stage.Stage;
 
 public class Main {
   public static void main(String[] args) {
     //MainWindow.appMain(args);
     DeckOfCards deck = new DeckOfCards();
+    Collection<PlayingCard> dealt = deck.dealHand(40);
+    for (PlayingCard card : dealt) {
+      System.out.println(card.getFace() + ", " + card.getSuit());
+    }
   }
 }
