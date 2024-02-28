956a43d6c78554bff0cd6fcf235878ae9b967c9b
ludlun04 <61392837+ludlun04@users.noreply.github.com>
2024-02-28 10:26:11 +0100

    added right and center pane

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/MainWindow.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/MainWindow.java
@@ -1,8 +1,18 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
+import javafx.scene.control.Button;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application{
@@ -15,9 +25,35 @@ public class MainWindow extends Application{
    */
   @Override
   public void start(Stage stage) throws Exception {
-    BorderPane rootNode = new BorderPane();
 
-    Scene scene = new Scene(rootNode, 1, 100);
+
+    Button dealHandButton = new Button("Deal Hand");
+    Button checkHandButton = new Button("Check Hand");
+
+    VBox rightPane = new VBox();
+    rightPane.getChildren().addAll(dealHandButton, checkHandButton);
+    rightPane.setAlignment(Pos.CENTER);
+    rightPane.setSpacing(20);
+    rightPane.setPadding(new Insets(20));
+
+    TextArea t1 = new TextArea("test");
+    TextArea t2 = new TextArea("test");
+    TextArea t3 = new TextArea("test");
+    TextArea t4 = new TextArea("test");
+    TextArea t5 = new TextArea("test");
+
+    ArrayList<TextArea> cardsAsText = new ArrayList<>(Arrays.asList(t1, t2, t3, t4, t5));
+
+    VBox centerPane = new VBox();
+    centerPane.getChildren().addAll(cardsAsText);
+    centerPane.setAlignment(Pos.CENTER);
+
+
+    BorderPane rootNode = new BorderPane();
+    rootNode.setRight(rightPane);
+    rootNode.setCenter(centerPane);
+
+    Scene scene = new Scene(rootNode, 200, 300);
     stage.setScene(scene);
     stage.setTitle("Card game for professional poker players");
     stage.show();
