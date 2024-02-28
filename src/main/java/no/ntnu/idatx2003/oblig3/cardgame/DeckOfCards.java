7a17d7959a6883ce053c2a7645490d729112f809
ludlun04 <61392837+ludlun04@users.noreply.github.com>
2024-02-28 10:55:49 +0100

    fixed deck so cards are unique and deal hand never gives the same hand twice

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
@@ -1,5 +1,6 @@
 package no.ntnu.idatx2003.oblig3.cardgame;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Scanner;
 public class DeckOfCards {
@@ -20,14 +21,15 @@ public class DeckOfCards {
 
   private void instantiateDeck() {
 
-    int suitsCount = this.suits.length;
-
-    for (int i = 0; i < this.count; i++) {
-      int value = (int) (Math.random() * (maxVal + 1 - minVal) + minVal);
-      int suitIndex = (int) (Math.random() * suitsCount);
-      char suit = this.suits[suitIndex];
-      cards[i] = new PlayingCard(suit, value);
+    int deckIndex = 0;
+    for (int i = 0; i < this.suits.length; i++) {
+      for (int j = minVal; j <= maxVal; j++) {
+        PlayingCard newCard = new PlayingCard(this.suits[i], j);
+        this.cards[deckIndex] = newCard;
+        deckIndex++;
+      }
     }
+
   }
 
   public Collection<PlayingCard> dealHand(int amountOfCards) {
@@ -35,9 +37,16 @@ public class DeckOfCards {
       throw new IllegalArgumentException("Must be in range 1-" + this.count);
     }
     ArrayList<PlayingCard> cardsToBeDealt= new ArrayList<>();
+    ArrayList<PlayingCard> cardsCopy = new ArrayList<>(Arrays.asList(this.cards));
+
+    System.out.println(cardsCopy.size());
+
     for (int i = 0; i < amountOfCards; i++) {
-      int randIndex = (int) (Math.random() * (this.count));
-      cardsToBeDealt.add(this.cards[randIndex]);
+      int randIndex = (int) (Math.random() * (cardsCopy.size()));
+      PlayingCard randomCard = cardsCopy.get(randIndex);
+      cardsToBeDealt.add(randomCard);
+      cardsCopy.remove(randIndex);
+
     }
     return cardsToBeDealt;
   }
